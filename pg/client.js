18c7665e4d4fb5495c5d1e1db0bf355af34029f6
unknown <ericryanbowser@gmail.com>
2024-04-25 05:21:28 -0600

    update flow

--- pg/client.js
+++ pg/client.js
@@ -1,16 +1,24 @@
 ﻿const {Client} = require('pg');
 const config = require("dotenv").config();
 const path = require('path');
+/*
 const Pinecone = require("@pinecone-database/pinecone");
+*/
 
 // Change .env based on local dev or prod
+/*
 const env = path.resolve(__dirname, '.env');
+*/
+/*
 const options = {
 	path: env
 };
+*/
 
+/*
 const connectionString =
 	`postgres://${config.parsed.DB_USER}:${config.parsed.DB_PASSWORD}@${config.parsed.DB_SERVER}:${config.parsed.DB_PORT}/postgres`;
+*/
 
 async function connectLocalPostgres() {
 	let client = null;
