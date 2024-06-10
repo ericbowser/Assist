30fa70cc347d89b5e2b44be8ad06c759e06de2c2
unknown <ericryanbowser@gmail.com>
2024-06-10 10:51:22 -0600

    stuff

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const {getDataV2} = require("@alpacahq/alpaca-trade-api/dist/resources/datav2/re
 */
 
 const alpaca = new Alpaca({
-    keyId: process.env.ALPACA_API_KEY,
-    secretKey: process.env.ALPACA_API_SECRET,
+    keyId: process.env.ALPACA_PAPER_API_KEY,
+    secretKey: process.env.ALPACA_PAPER_API_SECRET,
     paper: true,
 })
 
