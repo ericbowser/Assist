b9e680538bc79d0368c9941bbc1654006551631a
Eric Bowser <ericryanbowser@gmail.com>
2025-09-09 12:20:22 -0600

    ..

---
+++ crypto/alpacaPaperApi.js
@@ -0,0 +1,83 @@
+﻿"use strict";
+
+/**
+ * This example shows how to use the Alpaca Data V2 websocket to subscribe to events.
+ * The socket is available under the `data_steam_v2` property on an Alpaca instance.
+ * There are separate functions for subscribing (and unsubscribing) to trades, quotes and bars as seen below.
+ */
+
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+const API_KEY = process.env.ALPACA_API_KEY;
+const API_SECRET = process.env.ALPACA_API_SECRET;
+
+const RestApi = () => {
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+    const start = '2024-04-29';
+    const end = '2024-03-29';
+    const symbols = ['BTC', 'ETH', 'SOL'].join('&');
+    const fullUrl = `${process.env.ALPACA_PAPER_URL}&${start}&${end}}` + symbols;
+    
+    fetch(process.env.ALPACA_BASE_URL, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
+export default class AlpacaDataStream {
+    constructor({apiKey, secretKey, feed}) {
+        this.alpaca = new Alpaca({
+            keyId: apiKey,
+            secretKey,
+            feed,
+        });
+
+        const socket = this.alpaca.data_stream_v2;
+
+        socket.onConnect(function () {
+            console.log("Connected");
+            socket.subscribeForDailyBars();
+        });
+
+        socket.onError((err) => {
+            console.log(err);
+        });
+
+        socket.onStockTrade((trade) => {
+            console.log(trade);
+        });
+
+        socket.onStockQuote((quote) => {
+            console.log(quote);
+        });
+
+        socket.onStockBar((bar) => {
+            console.log(bar);
+        });
+
+        socket.onStatuses((s) => {
+            console.log(s);
+        });
+
+        socket.onStateChange((state) => {
+            console.log(state);
+        });
+
+        socket.onDisconnect(() => {
+            console.log("Disconnected");
+        });
+
+        socket.connect();
+
+        // unsubscribe from FB after a second
+        setTimeout(() => {
+            socket.unsubscribeFromTrades(["FB"]);
+        }, 1000);
+    }
+}
+
+let stream = new AlpacaDataStream({
+    apiKey: API_KEY,
+    secretKey: API_SECRET,
+    feed: "sip",
+    paper: true,
+});
---
+++ crypto/binanceSpotApi.js
@@ -0,0 +1,27 @@
+﻿const { CoinMClient } = require('binance');
+const { MainClient } = require('binance');
+const { Spot } = require('@binance/connector')
+
+
+const keys = {
+    apiKey: process.env.BINANCE_TEST_API_KEY,
+    secretKey: process.env.BINANCE_TEST_API_SECRET_KEY
+}
+
+// Get account information
+async function getAccount() {
+    try {
+     /*   const mainClient = new MainClient({
+            api_key: keys.apiKey,
+            api_secret: keys.secretKey,
+            baseUrl: 'https://testnet.binance.vision/api'
+        })*/
+        const spot = new Spot(keys.apiKey, keys.secretKey, {Timeout: 1000, baseURL: 'https://testnet.binance.vision/api'});
+        
+        console.log(spot)
+    } catch(err) {
+        console.log('error ', err)
+    }
+}
+
+module.exports = {getAccount};
